f83c119fa1cd92a83da31bb93bac7a9c3a2cd213
McMmie <tharausamuel01@gmail.com>
2024-04-17 23:13:31 +0000

    using files from the previous project

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+manages the api authentication
+"""
+from flask import request
+from typing import List, TypeVar
+import fnmatch
+
+
+class Auth:
+    """
+    a class to manage the api
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns a boolean
+        """
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        for item in excluded_paths:
+            if fnmatch.fnmatch(path, item):
+                return False
+            else:
+                return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        validates all requests to validate the API
+        """
+        header = request.headers.get('Authorization')
+        if header is None or request is None:
+            return None
+        else:
+            return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns none
+        """
+        return None
